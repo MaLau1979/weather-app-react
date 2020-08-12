f82350b5f5b9e89b71e2d5bce27f277f4a6ca1f6
MaLau1979 <valvanoster@gmail.com>
2020-08-12 19:59:33 -0300

    first commit

---
+++ src/components/Location.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Location = (props) => {
+    console.log(props);
+    debugger;
+    
+    return (
+        <div><h1>Buenos Aires</h1></div>);
+};
+
+export default Location;
---
+++ src/components/WeatherData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WeatherExtraInfo from './WeatherExtraInfo';
+import WeatherTemperature from './WeatherTemperature';
+
+const WeatherData = () => (
+    <div>
+        <WeatherTemperature />
+        <WeatherExtraInfo />
+        
+    </div>
+);
+
+export default WeatherData;
